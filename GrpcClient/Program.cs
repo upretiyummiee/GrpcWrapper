b5f1507b6aeb3cb1ec2849fd063c3014c7ecb9b0
upretiyummiee <upretiyummiee@hotmail.com>
2021-07-19 22:05:47 +0545

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using customstream;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Streamer.StreamerClient(channel);
+            var tokensource = new CancellationTokenSource(TimeSpan.FromSeconds(9));
+            var result = client.GetCount(new Empty(),cancellationToken:tokensource.Token);
+            try
+            {
+                await foreach (var item in result.ResponseStream.ReadAllAsync(tokensource.Token)) 
+                {
+                    Console.WriteLine("  "+item.C);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message.ToString()); }
+            Console.WriteLine(result);
+            _ = Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcServer/Services/StreamerService.cs
@@ -0,0 +1,24 @@
+﻿using customstream;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class StreamerService: Streamer.StreamerBase
+    {
+        public override async Task GetCount(Empty request, IServerStreamWriter<Count> responseStream, ServerCallContext context)
+        {
+            var count = 0;
+            while (!context.CancellationToken.IsCancellationRequested) {
+                await responseStream.WriteAsync(new Count { C = count });
+                count++;
+                Thread.Sleep(200);
+            } 
+        }
+    }
+}
